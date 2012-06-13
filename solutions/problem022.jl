6451b235dbc01ff0e6ce7ef22e1abe7d63278edb
Ian Macalinao <ianmacalinao@gmail.com>
2012-06-13 01:30:47 -0500

    Problem 22

---
+++ solutions/problem022.jl
@@ -0,0 +1,31 @@
+function sort_strings!(arr)
+    
+end
+
+function run()
+    names_list = readall(open("../resources/names.txt"))
+    names = split(names_list, ",", false)
+    for i = 1:size(names, 1)
+        names[i] = names[i][2:strlen(names[i]) - 1]
+    end
+    names_sort = sort(names)
+
+    total = 0
+    capa = int16('A') - 1
+    for i = 1:size(names_sort, 1)
+        name = names_sort[i]
+        nt = 0
+        for j = 1:strlen(name)
+            nt += int16(name[j]) - capa
+        end
+        total += i * nt
+    end
+    return total
+end
+
+i = time()
+s = run()
+i = time() - i
+
+println("Answer: $s")
+println("Executed in $i seconds")
